71a9ce55457bac5c34c83eac5eef682b3ebbeed2
Michael Orji <princemikeorji@gmail.com>
2022-04-17 09:16:45 +0100

    created models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from shortuuidfield import ShortUUIDField
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    image = models.ImageField()
+    price = models.DecimalField(max_digits=9,decimal_places=2 )
+    date = models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    transaction_id = ShortUUIDField()
+    complete = models.BooleanField(default=False)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_price(self):
+        total = self.product.price * self.quantity
+        return total
+
+    def __str__(self):
+        return str(self.id)
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def store(request):
+    products = Product.objects.all().order_by('-date')
+    return render(request, 'index.html', {'products': products})
